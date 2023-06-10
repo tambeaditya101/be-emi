2de1818b304ec6319d1396219f5f16cd4cb0e1d6
tambeaditya101 <tambeaditya101@gmail.com>
2023-06-10 15:03:00 +0530

    emi-backend

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const { connection } = require("./db");
 const { userRouter } = require("./routes/User.route");
-// const { postRouter } = require("./routes/Post.route");
 const cors = require("cors");
-// const { auth } = require("./middleware/auth.middleware");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/users", userRouter);
-
-// app.use("/post", auth, postRouter);
 
 app.listen(process.env.PORT, async () => {
   try {
